0428b820e234815540be50b4cd51fd0645a0edb3
Visualize-ML <105787223+Visualize-ML@users.noreply.github.com>
2022-07-11 08:29:44 -0400

    Add files via upload

---
+++ Book4_Ch01_Python_Codes/Bk3_Ch1_05.py
@@ -0,0 +1,17 @@
+
+###############
+# Authored by Weisheng Jiang
+# Book 4  |  From Basic Arithmetic to Machine Learning
+# Published and copyrighted by Tsinghua University Press
+# Beijing, China, 2022
+###############
+
+# Bk3_Ch1_05.py
+
+import numpy as np
+
+# define a column vector
+a = np.array([[2], [2]])
+
+b = 2*a
+c = -1.5*a
