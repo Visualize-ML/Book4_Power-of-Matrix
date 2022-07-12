00ae4159c2df49defd04af89efc15985601afe25
Visualize-ML <105787223+Visualize-ML@users.noreply.github.com>
2022-07-12 08:42:32 -0400

    Add files via upload

---
+++ Book4_Ch03_Python_Codes/Bk4_Ch3_15.py
@@ -0,0 +1,16 @@
+
+###############
+# Authored by Weisheng Jiang
+# Book 4  |  From Basic Arithmetic to Machine Learning
+# Published and copyrighted by Tsinghua University Press
+# Beijing, China, 2022
+###############
+
+# Bk4_Ch3_15.py
+
+import numpy as np
+A = np.array([[3, 1], 
+              [2, 4]])
+
+# calculate determinant of A
+det_A = np.linalg.det(A)
