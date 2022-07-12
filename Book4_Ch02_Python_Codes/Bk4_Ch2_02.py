7ccc54315fb44cb814a692c0819aa435f9824695
Visualize-ML <105787223+Visualize-ML@users.noreply.github.com>
2022-07-12 06:27:24 -0400

    Add files via upload

---
+++ Book4_Ch02_Python_Codes/Bk4_Ch2_02.py
@@ -0,0 +1,19 @@
+
+###############
+# Authored by Weisheng Jiang
+# Book 4  |  From Basic Arithmetic to Machine Learning
+# Published and copyrighted by Tsinghua University Press
+# Beijing, China, 2022
+###############
+
+# Bk4_Ch2_02.py
+
+import numpy as np
+a = np.array([[2,3],
+              [3,4]])
+
+b = np.array([[3,4],
+              [5,6]])
+
+a_@_b = np.dot(a,b)
+# a@b
---
+++ Book4_Ch02_Python_Codes/Bk4_Ch2_03.py
@@ -0,0 +1,19 @@
+
+###############
+# Authored by Weisheng Jiang
+# Book 4  |  From Basic Arithmetic to Machine Learning
+# Published and copyrighted by Tsinghua University Press
+# Beijing, China, 2022
+###############
+
+# Bk4_Ch2_03.py
+
+import numpy as np
+a = np.array([[1,2],
+              [3,4]])
+
+b = np.array([[3,4],
+              [5,6]])
+
+a_dot_b = np.vdot(a,b)
+# [1,2,3,4]*[3,4,5,6].T
